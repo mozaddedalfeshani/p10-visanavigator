e13f6ce5f89eff12914949f12389689a8259e72d
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-04 04:11:26 +0600

    Enhance PageNotFound component with a 404 message and improved styling

--- client/src/components/utils/PageNotFound.jsx
+++ client/src/components/utils/PageNotFound.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <div>
-      <Link to="/" className="btn">
+    <div className="card p-10">
+      <h1>404 Page Not Found</h1>
+      <Link to="/" className="btn btn-warning my-10">
         Go back to Home
       </Link>
     </div>
