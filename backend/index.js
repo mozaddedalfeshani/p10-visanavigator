69de3a22041865304f472876098b01f0f323945d
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-03 21:51:21 +0600

    Set up Express server with CORS and JSON middleware; establish MongoDB connection and update server listening logic

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,31 @@
 const { MongoClient } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
 
+const port = 8000;
 // Replace the uri string with your connection string.
 const uri = "mongodb://localhost:27017";
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const client = new MongoClient(uri);
 
 async function run() {
   try {
     const result = await client.connect();
-    console.log(result);
+
     const database = client.db("visaease").collection("latestCards");
 
     const show = await database.find().toArray();
-    console.log(show);
+    // console.log(show);
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    await client.close();
   }
 }
 run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
