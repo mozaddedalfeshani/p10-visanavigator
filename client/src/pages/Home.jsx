f874faf07a55ad4e80afb657319fbd153507709d
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-03 20:51:07 +0600

    Implement routing and navigation structure with Home page and NavBar component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import NavBar from "../components/Home/NavBar";
+import { Outlet } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+  
+      <NavBar />
+      <Outlet />
+    </div>
+  );
 }
