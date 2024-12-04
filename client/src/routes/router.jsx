a4a18bea2594fb68f36b2c1741cd2c3e7680e077
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-04 22:48:49 +0600

    Add routes for AddVisa, MyAddedVisas, and MyVisaApplications pages; wrap with PrivateProvider for authentication protection

--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -7,6 +7,9 @@ import Profile from "../components/Auth/Profile";
 import PageNotFound from "../components/utils/PageNotFound";
 import MyProfile from "../pages/MyProfile";
 import PrivateProvider from "../providers/PrivateProvider";
+import AddVisa from "../pages/AddVisa";
+import MyAddedVisas from "../pages/MyAddedVisas";
+import MyVisaApplications from "../pages/MyVisaApplications";
 
 const router = createBrowserRouter([
   {
@@ -31,10 +34,33 @@ const router = createBrowserRouter([
       },
       {
         path: "/profilePage",
-
         element: (
           <PrivateProvider>
             <MyProfile />
+          </PrivateProvider>
+        ),
+      },
+      {
+        path: "/addVisa",
+        element: (
+          <PrivateProvider>
+            <AddVisa />
+          </PrivateProvider>
+        ),
+      },
+      {
+        path: "/myAddedVisa",
+        element: (
+          <PrivateProvider>
+            <MyAddedVisas />
+          </PrivateProvider>
+        ),
+      },
+      {
+        path: "/myVisaApplication",
+        element: (
+          <PrivateProvider>
+            <MyVisaApplications />
           </PrivateProvider>
         ),
       },
