0a7f52fc8b7a0bf47e6e4725f0ab4afda998f6ce
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-04 22:40:04 +0600

    Add email/password sign-in functionality and user profile update; implement authentication state listener in AuthProvider

--- client/src/providers/AuthProvider.jsx
+++ client/src/providers/AuthProvider.jsx
@@ -1,9 +1,12 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import {
   getAuth,
   GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
+  updateProfile,
 } from "firebase/auth";
 import app from "../services/authService";
 
@@ -11,11 +14,23 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
-
-  //function to sign in with Google
   const auth = getAuth(app);
   const [user, setUser] = useState(null);
   const googleProvider = new GoogleAuthProvider();
+
+  // Listen for authentication state to change.
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(app), (user) => {
+      setUser(user); // Set the user state when authentication state changes
+      setLoading(false); // Set loading to false when authentication state is determined
+    });
+    return () => {
+      unsubscribe(); // Cleanup the subscription on component unmount
+    };
+  }, []);
+
+  //function to sign in with Google
+
   const googleSignIn = () => {
     setLoading(true);
     signInWithPopup(auth, googleProvider)
@@ -39,6 +54,44 @@ const AuthProvider = ({ children }) => {
       });
   };
 
+  // Update user profile
+  const updateUserProfile = (displayName, photoURL) => {
+    const auth = getAuth(app);
+    if (auth.currentUser) {
+      updateProfile(auth.currentUser, {
+        displayName,
+        photoURL,
+      })
+        .then(() => {
+          setUser({ ...auth.currentUser, displayName, photoURL }); // Update the user state with the new profile information
+        })
+        .catch((error) => {
+          notify("Error updating profile: " + error.message); // Notify on profile update error
+        });
+    }
+  };
+
+  // Sign in with email and password
+  const signInUser = (email, password) => {
+    return new Promise((resolve, reject) => {
+      if (!email) {
+        reject("Email is required");
+        return;
+      }
+      signInWithEmailAndPassword(getAuth(app), email, password)
+        .then((result) => {
+          setUser(result.user); // Set the user state with the authenticated user
+          notify("Successfully logged in with email and password"); // Notify on successful email/password login
+          resolve(result.user); // Resolve the promise with the user data
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          notify("Error signing in: " + errorMessage); // Notify on wrong credentials
+          reject(errorMessage); // Reject the promise with the error message
+        });
+    });
+  };
+
   const contextValue = {
     user,
     setUser,
@@ -46,6 +99,8 @@ const AuthProvider = ({ children }) => {
     signOutUser,
     loading,
     setLoading,
+    updateUserProfile,
+    signInUser,
   };
   return (
     <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
