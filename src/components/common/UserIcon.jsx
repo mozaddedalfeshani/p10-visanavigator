0bd551c581c3d4f741f58017a678a7c32643eae3
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-06 03:35:55 +0600

    Add react-tooltip for user avatar hover effect and update package dependencies

--- src/components/common/UserIcon.jsx
+++ src/components/common/UserIcon.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css"; // Import styles for the tooltip
 
 const UserIcon = ({ user }) => {
   const firstName = user?.displayName?.split(" ")[0] || "Anonymous";
+
   return (
     <div className="flex-row flex gap-3 shadow-sm items-center justify-around">
       <button className="flex flex-row justify-center items-center gap-1">
         <div className="avatar online">
           <div className="w-10 rounded-full">
+            {/* Add data-tooltip-id to trigger the Tooltip */}
             <img
               src={user?.photoURL || "default-avatar.png"}
               alt={`${user?.displayName || "User"}'s avatar`}
-              title={user?.displayName || "User"}
+              data-tooltip-id="user-tooltip" // ID for tooltip
+              data-tooltip-content={user?.displayName || "Anonymous"} // Tooltip content
             />
+            <Tooltip id="user-tooltip" place="top" offset={10} />{" "}
+            {/* Tooltip configuration */}
           </div>
         </div>
         <p>Logout</p>
