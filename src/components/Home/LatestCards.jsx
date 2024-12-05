2dffdfa1d535bdb19d159369e9ded029572c1545
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-06 03:42:34 +0600

    Add react-simple-typewriter for animated text in LatestCards component and update package dependencies

--- src/components/Home/LatestCards.jsx
+++ src/components/Home/LatestCards.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Card from "../common/Card";
+import { Typewriter } from "react-simple-typewriter";
 
 const LatestCards = () => {
   const navigate = useNavigate();
@@ -16,7 +17,25 @@ const LatestCards = () => {
 
   return (
     <div className="flex justify-center items-center my-8 flex-col">
-      <h1 className="text-4xl font-bold my-4">Latest Visa </h1>
+      {/* Animated Text Section */}
+      <h1 className="text-4xl font-bold my-4">
+        <span style={{ color: "" }}>
+          <Typewriter
+            words={[
+              "Latest Visa Cards",
+              "See the Latest Updates",
+              "Explore New Visa Options",
+            ]}
+            loop={true} // Infinite loop
+            cursor
+            cursorStyle="|"
+            typeSpeed={100}
+            deleteSpeed={60}
+            delaySpeed={1000}
+          />
+        </span>
+      </h1>
+      {/* Visa Cards Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto">
         {visaCards.map((visa) => (
           <Card key={visa._id} visaCard={visa} />
